91b6fd7ec344a96042828364cab4cbade19b599f
jayricka <jayricka17@gmail.com>
2023-06-07 18:30:01 +0300

    str format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+# Iterate through lowercase letters from 'a' to 'z' excluding 'q' and 'e'
 for letter in range(97, 123):
     if chr(letter) not in ['q', 'e']:
         print("{}".format(chr(letter)), end='')
