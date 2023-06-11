a9f63a487da9a354696d4db947904d26d67d6d44
jayricka <jayricka17@gmail.com>
2023-06-11 13:27:02 +0300

    count and list argc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
-
-a = 10
-b = 5
-
-result_add = add(a, b)
-result_subtract = sub(a, b)
-result_multiply = mul(a, b)
-result_divide = div(a, b)
-
 if __name__ == "__main__":
-    print(f"{a} + {b} = {result_add}")
-    print(f"{a} - {b} = {result_subtract}")
-    print(f"{a} * {b} = {result_multiply}")
-    print(f"{a} / {b} = {result_divide}")
+    """Print the addition, subtraction, multiply and division of 10 and 5."""
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
