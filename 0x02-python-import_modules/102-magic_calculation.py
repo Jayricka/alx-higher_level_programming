6b6773d8193f8b1edb1aee89544532b539249f82
jayricka <jayricka17@gmail.com>
2023-06-11 22:01:07 +0300

    calc func

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
-     """Match bytecode provided by Holberton School."""
-    add = __import__('magic_calculation_102').add
-    sub = __import__('magic_calculation_102').sub
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
-
-        for i in range(4, 7):
+        for i in range(4, 6):
             c = add(c, i)
+        return (c)
 
-        return c
-
-    return sub(a, b)
+    else:
+        return(sub(a, b))
