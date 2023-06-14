abb47cad527986e845b352b28c59098fb650e6af
jayricka <jayricka17@gmail.com>
2023-06-14 11:00:25 +0300

    square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    # Create a new matrix with the same size as the input matrix
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            # Square each value and add it to the new row
+            new_row.append(num ** 2)
+        # Add the new row to the new matrix
+        new_matrix.append(new_row)
+    return new_matrix
