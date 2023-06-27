dbabc58ec9a3c52ec2b32743d35a6708adc1a3aa
jayricka <jayricka17@gmail.com>
2023-06-27 03:34:42 +0300

    errors except

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for element in my_list[:x]:
+            if isinstance(element, int):
+                print("{:d}".format(element), end="")
+                count += 1
+        print()
+    except IndexError:
+        pass
+    return count
