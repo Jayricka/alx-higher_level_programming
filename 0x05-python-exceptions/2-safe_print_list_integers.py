2900887d1ac25d7b0390bf1dd0d3edd62dd8e866
jayricka <jayricka17@gmail.com>
2023-06-27 04:55:39 +0300

    magic calcs

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import traceback
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0  # Counter for the number of integers printed
@@ -8,7 +9,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
     except IndexError as error:
-        print("\nIndexError: list index out of range")
+        print("\nIndexError: list index out of range\n")
+        traceback.print_exc()
         raise error
     finally:
         print()
