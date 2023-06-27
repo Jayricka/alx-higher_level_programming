197ddd79cd70318c90aa14b70fb913a2e4813b62
jayricka <jayricka17@gmail.com>
2023-06-27 05:23:15 +0300

    bugs

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-
-import dis
-
 def magic_calculation(a, b):
     result = 0
-
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            else:
-                result += (a ** b) / i
-        except Exception as e:
+            result += a ** b / i
+        except Exception:
             result = b + a
             break
-
     return result
-
-bytecode = dis.Bytecode(magic_calculation)
-for instruction in bytecode:
-    print(instruction)
