cf2274ac9de5d8ace6f43ba39eddb3e7d35ca2e8
jayricka <jayricka17@gmail.com>
2023-07-12 08:47:28 +0300

    sorted asc list class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""Defines Mylist class"""
+"""Defines an inherited list class Mylist. """
 
 
 class MyList(list):
-    """A custom list class that inherits from the built-in list class.
+    """A custom list class that inherits from the built-in list class."""
 
-    Public instance method:
-        - print_sorted(): Prints the list in sorted (ascending) order.
-
-    Inherits from:
-        list
-    """
 
     def print_sorted(self):
         """Prints the list in sorted (ascending) order."""
