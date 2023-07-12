816790decb3f9b924f24824af6df8016121c8615
jayricka <jayricka17@gmail.com>
2023-07-12 23:19:02 +0300

    fix bugs

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Read a text file and print its content to stdout."""
-
+"""Defines a text file and print its content to stdout."""
 
 
 def read_file(filename=""):
-    """Read the content of a text file and print it to stdout."""
-    with open(filename, encoding='utf-8') as file:
-        print(file.read(), end='')
+    """Print the content of a text file and print it to stdout."""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
