78d9958a393e2080e12366bb6d9af9bcbf3a1554
jayricka <jayricka17@gmail.com>
2023-07-19 08:19:07 +0300

    Square updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 """Square module"""
+
 from models.rectangle import Rectangle
 
 
@@ -29,6 +31,21 @@ class Square(Rectangle):
             'x': self.x,
             'y': self.y
         }
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes to the instance"""
+        if args and len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the Square instance"""
