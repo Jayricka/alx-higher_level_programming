7cfd20f031a6257c67cb4bcc8487af5b8a84cd63
jayricka <jayricka17@gmail.com>
2023-09-13 05:58:20 +0300

    files execution

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const converter = require('./10-converter').converter;
 
-let myConverter = converter(10);
+function converter (base) {
+  return function (number) {
+    return number.toString(base);
+  };
+}
 
-console.log(myConverter(2));
-console.log(myConverter(12));
-console.log(myConverter(89));
-
-myConverter = converter(16);
-
-console.log(myConverter(2));
-console.log(myConverter(12));
-console.log(myConverter(89));
+exports.converter = converter;
