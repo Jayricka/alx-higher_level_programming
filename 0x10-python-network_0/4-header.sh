16b0d54e933ef82b4774dc86b55dae298e3160ca
jayricka <jayricka17@gmail.com>
2023-09-29 05:54:33 +0300

    catch me if you can

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a GET request with a custom header, and displays the body of the response.
+# This script sends a GET request to a URL with a custom header and displays the response body.
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
