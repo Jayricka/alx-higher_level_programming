26ceecb26b2dbe063dc31e2766039b7472f77f8a
jayricka <jayricka17@gmail.com>
2023-09-30 06:15:44 +0300

    debugging api

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header of the response.
 """
+A script that:
+- Takes in a URL,
+- Sends a request to the URL, and
+- Displays value of X-Request-Id variable found in header of response.
+"""
+
+import requests
 import sys
-import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+    if x_request_id:
+        print(x_request_id)
