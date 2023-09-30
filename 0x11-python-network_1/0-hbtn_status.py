50bbd4a6ce450479e372f318031a6276b9733221
jayricka <jayricka17@gmail.com>
2023-09-30 05:42:03 +0300

    backend

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-This script fetches https://alx-intranet.hbtn.io/status using urllib.
-It displays the response body in a specific format.
-"""
 
-import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == '__main__':
+    import urllib.request
 
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", body.decode('utf-8'))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
