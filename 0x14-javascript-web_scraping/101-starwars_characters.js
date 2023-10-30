b104eff87455482016050dcc29cb9717227bfe91
jayricka <jayricka17@gmail.com>
2023-10-30 23:34:49 +0300

    web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,35 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
 
-if (!movieId) {
-  console.error('Please provide a movie ID.');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
-  } else {
-    const movie = JSON.parse(body);
-    const characterUrls = movie.characters;
-
-    function fetchCharacter(index) {
-      if (index < characterUrls.length) {
-        request(characterUrls[index], (charError, charResponse, charBody) => {
-          if (charError) {
-            console.error('Error:', charError);
-          } else if (charResponse.statusCode !== 200) {
-            console.error(`Failed to retrieve character data. Status code: ${charResponse.statusCode}`);
-          } else {
-            const character = JSON.parse(charBody);
-            console.log(character.name);
-            fetchCharacter(index + 1);
-          }
-        });
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
+    }
+  });
+}
