e98341c0654afe0cc55371db862767490febb4d2
jayricka <jayricka17@gmail.com>
2023-11-01 22:13:53 +0300

    jquery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('#toggle_header').click(function() {
+    const header = $('header');
+    if (header.hasClass('red')) {
+      header.removeClass('red');
+      header.addClass('green');
+    } else {
+      header.removeClass('green');
+      header.addClass('red');
+    }
+  });
+});
